59b5433b7cd2f22c6be99999d70c0a05ef78cbed
Crystnade <priyanshuprasad2007@gmail.com>
2025-10-04 21:17:52 +0530

    updated

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    printf("hello world\n");
+    int a =24,b = 2;
+    printf("the value of a+b = %d",a+b);
+    
+    getch();
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    printf("hello world\n");
+    int a,b;
+    a=4,b=23;
+    printf("%d\n",a+b);
+    getch();
+    return 0;
+}
